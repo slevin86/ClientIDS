709f70cd1aec5677d840c1ec77382e098a39e46e
Davide <Davide@Davide-VAIO>
2013-05-15 10:36:21 +0200

    branca base

---
+++ src/pacchetti/Starter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacchetti;
+
+/**
+ *
+ * @author Davide
+ */
+public class Starter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJDialog ciao = new NewJDialog(null, true);
+        ciao.setVisible(true);
+        
+    }
+}
